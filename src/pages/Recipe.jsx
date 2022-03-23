34d6bc2bf6c39a551ee9c073cfab9508956cd45e
Aart Sharma <aarti.sharmabhl@gmail.com>
2022-03-23 11:26:40 +0530

    initial commit

---
+++ src/pages/Recipe.jsx
@@ -0,0 +1,98 @@
+import React, {useEffect,useState} from 'react'
+import styled from 'styled-components'
+import {useParams} from "react-router-dom"
+import { act } from 'react-dom/test-utils';
+
+
+function Recipe() {
+
+    let params= useParams();
+    const [details, setDetails] = useState({})
+    const [activeTab,setActiveTab] = useState("instructions")
+    console.log(details);
+
+    const fatchDetails = async()=>{
+        const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`);
+        const detailData =await data.json();
+
+        setDetails(detailData);
+
+    }
+
+    useEffect(()=>{
+        fatchDetails();
+    }, [params.name])
+
+  return (
+      <DeatailWrapper>
+    <div>
+        <h2>
+        {details.title}
+        </h2>
+        <img src= {details.image} alt="" />
+        </div>
+
+        <Info>
+            <Button className={activeTab === "instruction" ? "active" : ""} onClick={()=>setActiveTab("instruction")}>Instruction</Button>
+            <Button className={activeTab === "ingredients" ? "active" : ""} onClick={()=>setActiveTab("ingredients")}>Ingredients</Button>
+            {activeTab === "instruction" &&  (
+            <div>
+                <h3 dangerouslySetInnerHTML={{__html: details.summary}}></h3>
+                <h3 dangerouslySetInnerHTML={{__html: details.instructions}}></h3>
+            </div>)}
+            {
+                activeTab === "ingredients" &&( <ul>
+                    {
+                        details.extendedIngredients.map((ingredients)=>(
+                            <li key = {ingredients.id}>
+    
+                                {ingredients.original}
+    
+                            </li>
+                        ))
+                    }
+                </ul>)
+            }
+           
+        </Info>
+    </DeatailWrapper>
+  )
+}
+
+const DeatailWrapper = styled.div `
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+ .active{
+      background: linear-gradient(35deg, #494949, #313131);
+      color: white;
+  }
+
+  h2{
+      margin-bottom: 2rem;
+  }
+
+  li{
+      font-size: 1.2rem;
+      line-height: 2.5rem;
+  }
+
+  ul{
+      margin-top: 2rem;
+  }
+`;
+
+const Button = styled.button `
+  padding: 1rem 2rem;
+  color: #313131;
+  background: white;
+  display: flex;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+`;
+const Info = styled.div `
+margin-left: 10rem
+`
+
+export default Recipe
